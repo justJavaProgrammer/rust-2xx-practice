e98675b6d12a70690c02a8ac00cf392ebcb32b61
odeyalo <theprogrammerinfuture@gmail.com>
2024-11-11 21:48:22 +0200

    Finish practice 7

--- src/practice7.rs
+++ src/practice7.rs
@@ -3,7 +3,7 @@ fn main() {
     let triangles = 7;
     for i in 0..triangles
     {
-        for j in 0..(i+1)
+        for j in 0..(i + 1)
         {
             println!("{}{}", " ".repeat(triangles - j - 1), "*".repeat(2 * j + 1));
         }
