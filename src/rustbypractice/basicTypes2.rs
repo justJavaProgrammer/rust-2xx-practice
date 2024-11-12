88911c6024b8f2a3615ad5cce167ff81cb5cb086
odeyalo <theprogrammerinfuture@gmail.com>
2024-11-12 21:03:16 +0200

    RBP: finish basic types

---
+++ src/rustbypractice/basicTypes2.rs
@@ -0,0 +1,69 @@
+// https://practice.course.rs/basic-types/char-bool-unit.html
+#[test]
+fn test_4_1()
+{
+    let c1 = 'a';
+    assert_eq!(size_of_val(&c1),4);
+
+    let c2 = '中';
+    assert_eq!(size_of_val(&c2),4);
+
+    println!("Success!");
+}
+#[test]
+fn test_4()
+{
+    // Make it work
+    fn main() {
+        let c1 = '中';
+        print_char(c1);
+    }
+    fn print_char(c : char) {
+        println!("{}", c);
+    }
+}
+#[test]
+fn test_4_3()
+{
+    let _f: bool = false;
+
+    let _t = true;
+    if !_f {
+        println!("Success!");
+    }
+}
+#[test]
+fn test_4_4()
+{
+    let f = true;
+    let t = true && false;
+    assert_eq!(t, !f);
+
+    println!("Success!");
+}
+#[test]
+fn test_4_5()
+{
+    let _v: () = ();
+
+    let v = ();
+    assert_eq!(v, implicitly_ret_unit());
+
+    println!("Success!");
+
+    fn implicitly_ret_unit() {
+        println!("I will return a ()");
+    }
+
+    // Don't use this one
+    fn explicitly_ret_unit() -> () {
+        println!("I will return a ()");
+    }
+}
+#[test]
+fn test_4_6()
+{
+    let unit: () = ();
+    assert!(size_of_val(&unit) == 0);
+    println!("Success!");
+}
---
+++ src/rustbypractice/basicTypes3.rs
@@ -0,0 +1,34 @@
+// https://practice.course.rs/basic-types/statements-expressions.html
+#[test]
+fn test_4_1()
+{
+    let v = {
+        let mut x = 1;
+        x + 2
+    };
+
+    assert_eq!(v, 3);
+
+    println!("Success!");
+}
+#[test]
+fn test_4_2()
+{
+    let v = {let x = 3; x};
+
+    assert!(v == 3);
+
+    println!("Success!");
+}
+#[test]
+fn test_4_3()
+{
+    let s = sum(1 , 2);
+    assert_eq!(s, 3);
+
+    println!("Success!");
+
+    fn sum(x: i32, y: i32) -> i32 {
+        x + y // removed ;
+    }
+}
