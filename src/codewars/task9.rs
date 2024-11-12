b038cab00487a11ab679c2b26048389fda16f9aa
odeyalo <theprogrammerinfuture@gmail.com>
2024-11-12 20:21:48 +0200

    Hackerrank: Complete task 9

---
+++ src/codewars/task9.rs
@@ -0,0 +1,30 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+fn timeConversion(s: &str) -> String {
+    let mut hour: u8 = (&s[0..2]).parse().expect("Couldn't parse");
+    hour = if &s[8..9]=="P" {12 + hour % 12} else {hour % 12};
+    let hour = format!("{:0>2}{}", hour, &s[2..8]);
+    return hour;
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let s = stdin_iterator.next().unwrap().unwrap();
+
+    let result = timeConversion(&s);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
