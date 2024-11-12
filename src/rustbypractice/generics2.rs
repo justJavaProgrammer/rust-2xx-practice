d051fb8a354c602c7f58440634d7971134691227
odeyalo <theprogrammerinfuture@gmail.com>
2024-11-12 21:05:56 +0200

    RBP: finish generics

---
+++ src/rustbypractice/generics2.rs
@@ -0,0 +1,33 @@
+// https://practice.course.rs/generics-traits/const-generics.html
+#[test]
+fn test10_1()
+{
+    struct Array<T, const N: usize> {
+        data: [T; N]
+    }
+    let arrays = [
+        Array {
+            data: [1, 2, 3],
+        },
+        Array {
+            data: [1, 2, 3],
+        },
+        Array {
+            data: [1, 2, 3]
+        }
+    ];
+
+    println!("Success!");
+}
+#[test]
+fn test10_2()
+{
+    fn print_array<T: std::fmt::Debug>(arr: T) {
+        println!("{:?}", arr);
+    }
+    let arr = [1, 2, 3];
+    print_array(arr);
+
+    let arr = ["hello", "world"];
+    print_array(arr);
+}
