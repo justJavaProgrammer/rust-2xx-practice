0fdf1ac910983e4397661b44f5eb5085a7cc0f57
odeyalo <theprogrammerinfuture@gmail.com>
2024-11-12 20:06:17 +0200

    Hackerrank: Complete task 2

---
+++ src/codewars/task2.rs
@@ -0,0 +1,62 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+fn compareTriplets(a: &[i32], b: &[i32]) -> Vec<i32> {
+    let mut alice: i32 = 0;
+    let mut bob: i32 = 0;
+
+    for (al, bo) in a.iter().zip(b.iter()) {
+        if al > bo {
+            alice += 1;
+        } else if al == bo {
+            continue;
+        } else if al < bo {
+            bob += 1;
+        }
+    }
+
+    let v1 = vec![alice, bob];
+
+    return v1;
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let a: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let b: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = compareTriplets(&a, &b);
+
+    for i in 0..result.len() {
+        write!(&mut fptr, "{}", result[i]).ok();
+
+        if i != result.len() - 1 {
+            write!(&mut fptr, " ").ok();
+        }
+    }
+
+    writeln!(&mut fptr).ok();
+}
