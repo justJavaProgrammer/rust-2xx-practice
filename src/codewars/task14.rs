3d9c893f5eb472ab8de83ae38040a2bab78075b4
odeyalo <theprogrammerinfuture@gmail.com>
2024-11-12 20:52:19 +0200

    Hackerrank: Complete task 14

---
+++ src/codewars/task14.rs
@@ -0,0 +1,55 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+fn breakingRecords(scores: &[i32]) -> Vec<i32> {
+    let mut highest = scores[0];
+    let mut lowest = scores[0];
+    let mut count_highest = 0;
+    let mut count_lowest = 0;
+
+    for &score in scores.iter() {
+        if score > highest {
+            highest = score;
+            count_highest += 1;
+        } else if score < lowest {
+            lowest = score;
+            count_lowest += 1;
+        }
+    }
+    vec![count_highest, count_lowest]
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let scores: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = breakingRecords(&scores);
+
+    for i in 0..result.len() {
+        write!(&mut fptr, "{}", result[i]).ok();
+
+        if i != result.len() - 1 {
+            write!(&mut fptr, " ").ok();
+        }
+    }
+
+    writeln!(&mut fptr).ok();
+}
