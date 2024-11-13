60b81ea4ae2b8ba72aaaf5f5ccd58bb5c6d8869d
odeyalo <theprogrammerinfuture@gmail.com>
2024-11-13 22:00:20 +0200

    Done practice task 5

---
+++ src/practice5.rs
@@ -0,0 +1,18 @@
+fn print_envelope(size: usize) {
+    for i in 0..size {
+        for j in 0..size {
+            if i == 0 || i == size - 1 || j == 0 || j == size - 1 || i == j || i + j == size - 1 {
+                print!("*");
+            } else {
+                print!(" ");
+            }
+        }
+        println!();
+    }
+}
+
+#[test]
+fn main() {
+    let size = 30;
+    print_envelope(size);
+}
